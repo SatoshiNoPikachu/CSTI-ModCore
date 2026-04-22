1a69d898014e1ac9810e4a9a7a992be4e2ee8e10
Pikachu <1223188723@qq.com>
2026-04-23 05:53:59 +0800

    feat: 命名空间现在不允许包含'|'

--- ModCore/Data/ModData.cs
+++ ModCore/Data/ModData.cs
@@ -4,38 +4,48 @@ using System.Linq;
 namespace ModCore.Data;
 
 /// <summary>
-/// 模组数据
+/// 模组数据。
 /// </summary>
-/// <param name="ns">命名空间，不允许包含':'和'@'</param>
-/// <param name="rootPath">模组根目录</param>
+/// <param name="ns">命名空间，不允许包含':'和'@'以及'|'。</param>
+/// <param name="rootPath">模组根目录。</param>
 public class ModData(string ns, string rootPath)
 {
     /// <summary>
-    /// 命名空间
+    /// 命名空间。
     /// </summary>
-    public string Namespace { get; } = HasNamespace(ns) ? throw new ArgumentException(nameof(ns)) : ns;
+    public string Namespace { get; } = IsValidNamespace(ns) ? ns : throw new ArgumentException(nameof(ns));
 
     /// <summary>
-    /// 模组根目录
+    /// 模组根目录。
     /// </summary>
     public string RootPath { get; } = rootPath ?? throw new ArgumentNullException(nameof(rootPath));
 
     /// <summary>
-    /// 获取命名空间
+    /// 判断数据键中是否包含命名空间。
     /// </summary>
-    /// <param name="key">数据键，支持分隔符':'和'@'</param>
-    /// <returns>如果数据键存在命名空间，则返回命名空间，否则返回null</returns>
+    /// <param name="key">数据键，支持分隔符':'和'@'。</param>
+    /// <returns>是否包含命名空间。</returns>
     public static bool HasNamespace(string key)
     {
         return key.Any(c => c is ':' or '@');
     }
 
     /// <summary>
-    /// 获取数据
+    /// 是否是有效的命名空间。
     /// </summary>
-    /// <param name="key">不带命名空间的键</param>
-    /// <typeparam name="T">数据类型</typeparam>
-    /// <returns>如果键存在，则返回数据对象，否则返回null</returns>
+    /// <param name="ns">命名空间，不允许包含':'和'@'以及'|'。</param>
+    /// <returns>命名空间是否有效。</returns>
+    public static bool IsValidNamespace(string ns)
+    {
+        return !ns.Any(c => c is ':' or '@' or '|');
+    }
+
+    /// <summary>
+    /// 获取数据。
+    /// </summary>
+    /// <param name="key">不带命名空间的键。</param>
+    /// <typeparam name="T">数据类型。</typeparam>
+    /// <returns>如果键存在，则返回数据对象，否则返回null。</returns>
     public T? GetData<T>(string key)
     {
         return Database.GetData<T>($"{Namespace}:{key}");
